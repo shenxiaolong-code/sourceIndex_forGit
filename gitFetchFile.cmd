d03102e29e1fdcfba3011f2721c89ee11adb526a
xiaolosh <xiaolosh@cisco.com>
2020-01-02 18:08:25 +0800

    prompt user to add localGitRepo when it is not defined

--- gitFetchFile.cmd
+++ gitFetchFile.cmd
@@ -1,6 +1,13 @@
+@echo off
+::if not defined localGitRepo set localGitRepo=D:\sourceCode\jabberGit129
+if not defined localGitRepo (
+echo localGitRepo environment is not defined.
+echo define localGitRepo environment variable point to local git repo.
+echo e.g. set localGitRepo=D:\sourceCode\jabberGit129
+goto :eof
+)
 
 if not exist "%~dp2" md "%~dp2"
-set localGitRepo=D:\sourceCode\jabberGit129
 git.exe -C "%localGitRepo%" show "%~1" > "%~fs2"
 
 echo type "%~fs2"
